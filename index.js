5f89615854e04e8ab7a48f4c04f49aec2d5ba7b8
Paul Holden <pholden@stria.com>
2019-04-25 08:17:05 -0700

    Download all 5 contract types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const async = require('async');
 const sppull = require('sppull').sppull;
 const dotenv = require('dotenv');
 
@@ -11,16 +12,55 @@ const context = {
   }
 };
 
-const options = {
-  spRootFolder: process.env.SHAREPOINT_ROOT,
-  dlRootFolder: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files',
+let options = {
+  spRootFolder: '',
+  dlRootFolder: '',
   recursive: true,
   folderStructureOnly: false,
   ignoreEmptyFolders: false
 };
 
-sppull(context, options).then(() => {
-  console.log('success');
-}).catch(err => {
-  console.log(err);
+// Peek will download just the first folder-level, to test root folder names
+if (process.env.SHAREPOINT_PEEK) {
+  options.recursive = false;
+  options.folderStructureOnly = true;
+}
+
+const downloads = [
+  {
+    remote: 'Client Contracts  Current',
+    local: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files\\Client'
+  },
+  {
+    remote: 'NonDisclosure Agreements',
+    local: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files\\NDA'
+  },
+  {
+    remote: 'Vendor Contracts',
+    local: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files\\Vendor'
+  },
+  {
+    remote: 'Independent Contractor Agreements',
+    local: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files\\Contractor'
+  },
+  {
+    remote: 'Partnership Agreements',
+    local: '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\SharePoint Files\\Partner'
+  }
+];
+
+async.eachSeries(downloads, (dl, callback) => {
+  options.spRootFolder = dl.remote;
+  options.dlRootFolder = dl.local;
+  console.log(`${dl.remote} => ${dl.local}`);
+  sppull(context, options).then(() => {
+    console.log('Done');
+    callback();
+  }).catch(callback);
+}, err => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Completed all');
 });
